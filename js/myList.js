eba0c9979eac89622810cd348d2e508126bcd564
Kashyap Kokkiligadda <A00209673@mycambrian.ca>
2019-11-18 15:15:34 -0500

    first commit

---
+++ js/myList.js
@@ -0,0 +1,44 @@
+
+function textIdSelectorFunction(event) {
+    event.preventDefault();
+
+    const textIdSelector = document.querySelector('#text-ID');
+    const textvalue = textIdSelector.value.trim();
+
+    if (textvalue.length > 0) {
+        const randomID = (Date.now().toString(36).substr(2, 4) + performance.now().toString(36).replace('.','').substr(0, 4) + Math.random().toString(36).substr(3, 4)).toUpperCase();
+        const tempContent = document.querySelector('#item-template');
+        
+        
+        const itemContent = document.importNode(tempContent.content, true);
+        const inputID = itemContent.querySelector('#select-ID');
+        
+        const idValue = `textIdSelector-select-${randomID}`;
+        inputID.setAttribute('id', idValue);
+        const labelValue = itemContent.querySelector('label[for]');
+        labelValue.setAttribute('for', idValue);
+
+        labelValue.textContent = textvalue;
+        const listItems = document.querySelector('#list-items');
+        listItems.appendChild(itemContent);
+    }
+
+    textIdSelector.value = '';
+}
+function bindFuncToButton() {
+    let button = document.querySelector('#add-button');
+    button.addEventListener('click', textIdSelectorFunction);
+    
+}
+function show_img(checkbox){
+
+    if (checkbox.checked){
+        
+        document.getElementById("smiley").setAttribute("style","display:inline");
+}else{
+    
+    document.getElementById("smiley").setAttribute("style","display:none");
+}
+}
+
+bindFuncToButton();
